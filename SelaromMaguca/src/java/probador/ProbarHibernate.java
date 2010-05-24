b6e42dea7a41f9fd73506ae9b29045ed4a3c5e6c
mcdsx10 <mcdsx10@2fe2b151-1866-4bc7-5276-41e8786ae583>
2010-05-24 23:50:48 +0000

    clase cargo, empleado, y clase inicial de llamada a hibernate

---
+++ SelaromMaguca/src/java/probador/ProbarHibernate.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package probador;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ *
+ * @author isak
+ */
+public class ProbarHibernate {
+
+    private SessionFactory sessionFactory;
+    
+
+    public void setSessionFactory(SessionFactory sessionFactory){
+        
+        this.sessionFactory =sessionFactory;          
+    }
+
+    public void asociarSession(){
+
+        this.setSessionFactory(new Configuration().configure().buildSessionFactory());
+    }
+
+    public SessionFactory getSessionFactory() {
+
+        return sessionFactory;
+    }
+
+    
+
+}
