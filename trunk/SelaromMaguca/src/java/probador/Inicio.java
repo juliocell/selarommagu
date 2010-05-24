f1f9bb81406a187311dc48094a23ff9c9006c89f
mcdsx10 <mcdsx10@2fe2b151-1866-4bc7-5276-41e8786ae583>
2010-05-24 23:50:48 +0000

    clase cargo, empleado, y clase inicial de llamada a hibernate

---
+++ trunk/SelaromMaguca/src/java/probador/Inicio.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package probador;
+
+import datos.Cargo;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author isak
+ */
+public class Inicio {
+
+    public static void main(String Args[])
+    {
+        ProbarHibernate laSession = new ProbarHibernate();
+        laSession.asociarSession();
+        Session sesion = laSession.getSessionFactory().getCurrentSession();
+        sesion.beginTransaction();//comienzo la transaccion
+        Cargo cargo = new Cargo(1, "Cajero");
+        Cargo cargo1 = new Cargo(1, "Gerente");
+        Cargo cargo2 = new Cargo(1, "Administrador");
+        sesion.save(cargo);//salvo un cargo
+        sesion.save(cargo1);
+        sesion.save(cargo2);
+        
+        
+//TODO: terminar la prueba de hibernate para salvar y buscar un empleado y una categoria
+    }
+}
