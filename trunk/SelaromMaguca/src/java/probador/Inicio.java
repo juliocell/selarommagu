3ff11d130c3ba7bbee1698238388c3182d65e0e6
mcdsx10 <mcdsx10@2fe2b151-1866-4bc7-5276-41e8786ae583>
2010-05-25 00:49:20 +0000

    creado metodo de mostrarCargo que muestra los cargos almacenados en la tabla Cargo de la BD

--- trunk/SelaromMaguca/src/java/probador/Inicio.java
+++ trunk/SelaromMaguca/src/java/probador/Inicio.java
@@ -6,6 +6,7 @@
 package probador;
 
 import datos.Cargo;
+import java.util.List;
 import org.hibernate.Session;
 
 /**
@@ -20,15 +21,32 @@ public class Inicio {
         laSession.asociarSession();
         Session sesion = laSession.getSessionFactory().getCurrentSession();
         sesion.beginTransaction();//comienzo la transaccion
-        Cargo cargo = new Cargo(1, "Cajero");
-        Cargo cargo1 = new Cargo(2, "Gerente");
-        Cargo cargo2 = new Cargo(3, "Administrador");
+        Cargo cargo = new Cargo(4, "Cajero");
+        Cargo cargo1 = new Cargo(5, "Gerente");
+        Cargo cargo2 = new Cargo(6, "Administrador");
         sesion.save(cargo);//salvo un cargo
         sesion.save(cargo1);
         sesion.save(cargo2);
         sesion.getTransaction().commit();
+        Inicio ini = new Inicio();
+        ini.mostrarCargo();
+
+
         
         
 //TODO: terminar la prueba de hibernate para salvar y buscar un empleado y una categoria
+    }
+
+    public void  mostrarCargo(){
+        ProbarHibernate laSession = new ProbarHibernate();
+        laSession.asociarSession();
+        Session sesion = laSession.getSessionFactory().getCurrentSession();
+        sesion.beginTransaction();//comienzo la transaccion
+        List<Cargo> resultado = (List<Cargo>)sesion.createQuery("from Cargo").list();
+        sesion.getTransaction().commit();
+        for (Cargo cargo : resultado) {
+            System.out.println("Cargo: "+ resultado);
+        }
+       
     }
 }
