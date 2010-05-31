5df6c1377bcc4f8790799b1e6efecfa651c52315
mcdsx10 <mcdsx10@2fe2b151-1866-4bc7-5276-41e8786ae583>
2010-05-31 16:35:17 +0000

    clase cargoDaoImpltest con todas las pruebas listas y probadas

--- SelaromMaguca/src/java/modelo/CargoDaoImpl.java
+++ SelaromMaguca/src/java/modelo/CargoDaoImpl.java
@@ -5,12 +5,12 @@
 
 package modelo;
 
-import com.sun.org.apache.bcel.internal.generic.INSTANCEOF;
+//import com.sun.org.apache.bcel.internal.generic.INSTANCEOF;
 import datos.Cargo;
 import java.util.List;
 import org.hibernate.HibernateException;
-import org.hibernate.LockMode;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import probador.ProbarHibernate;
 
 /**
@@ -18,7 +18,7 @@ import probador.ProbarHibernate;
  * @author isak
  */
 public class CargoDaoImpl{
-//TODO: pendiente con la busqueda de empleados por cargo???
+
 
     public void salvarCargo(Cargo elCargo, ProbarHibernate pro){
         //ProbarHibernate laSession = new ProbarHibernate();
@@ -30,32 +30,32 @@ public class CargoDaoImpl{
         
     }
 
-    public boolean eliminarCargo(Object aBorrar){
+    public boolean eliminarCargo(Cargo aBorrar, ProbarHibernate pro){
 
-        ProbarHibernate laSession = new ProbarHibernate();
-        Session sesion = laSession.getSessionFactory().getCurrentSession();
+        Session sesion = pro.getSessionFactory().getCurrentSession();
         sesion.beginTransaction();//comienzo la transaccion
 
         try{
-
-            sesion.delete(aBorrar);
+            Cargo aux = (Cargo)sesion.load(Cargo.class,aBorrar.getIdCargo());
+            sesion.delete(aux);
+            sesion.getTransaction().commit();
+            //sesion.close();
             return true;
-
         }
         catch(HibernateException ex){
             System.out.println("ERROR al eliminar Cargo:" + ex.getMessage());
-            
+            sesion.getTransaction().rollback();
+            return false;
         }
         finally{
-            sesion.close();
-            return false;
+            
+                       
         }
     }
 
-    public void actualizarCargo(Cargo aActualizar){
-        ProbarHibernate laSession = new ProbarHibernate();
-        Session sesion = laSession.getSessionFactory().getCurrentSession();
-        sesion.beginTransaction();//comienzo la transaccion
+    public void actualizarCargo(Cargo aActualizar, ProbarHibernate pro){
+        Session sesion = pro.getSessionFactory().getCurrentSession();
+        sesion.beginTransaction();//comienzo la transaccion        
         sesion.update(aActualizar);
         sesion.close();
     }
@@ -77,30 +77,52 @@ public class CargoDaoImpl{
         return aux;
     }
 
+    public Cargo buscarCargoPorId(int id, ProbarHibernate pro){
+        Session sesion = pro.getSessionFactory().getCurrentSession();
+        sesion.beginTransaction();//comienzo la transaccion
+        
+        try
+        {
+            Cargo elCargo = (Cargo)sesion.load(Cargo.class, id);
+            return elCargo;
+        }
+        catch(HibernateException ex){
+            System.out.println("ERROR al buscar el Cargo:" + ex.getMessage());
+            return null;
+        }
+    }
+
     /**
      * Metodo que devuelve una lista de objectos tipo Cargo
      * @return
      */
-    public List<Cargo>  listaCargo()
+    public List<Cargo>  listaCargo(ProbarHibernate pro)
     {
-        ProbarHibernate laSession = new ProbarHibernate();
-        Session sesion = laSession.getSessionFactory().getCurrentSession();
+        Session sesion = pro.getSessionFactory().getCurrentSession();
         sesion.beginTransaction();//comienzo la transaccion
         List<Cargo> resultado = (List<Cargo>)sesion.createQuery("from Cargo").list();
         sesion.getTransaction().commit();
-
-        for (Cargo cargo : resultado)
+        if(resultado!=null)
         {
-            System.out.println("Cargo: "+ resultado);
-        }
+            for (Cargo cargo : resultado)
+            {
+                System.out.println("Cargo: "+ resultado);
+            }
 
-        return resultado;
+            return resultado;
+
+        }
+        else
+        {
+            return null;
+
+        }
     }
 
   
     
 
-
+//TODO: hacer las pruebas de actualizar, buscar y listarCargo
 
 }
 
--- SelaromMaguca/test/modelo/CargoDaoImplTest.java
+++ SelaromMaguca/test/modelo/CargoDaoImplTest.java
@@ -5,7 +5,7 @@
 
 package modelo;
 
-import ControladorBaseDatos.ProductoCategoria;
+
 import datos.Cargo;
 import java.util.List;
 import org.junit.After;
@@ -53,8 +53,10 @@ public class CargoDaoImplTest {
         pro.asociarSession();
         instance.salvarCargo(elCargo,pro);
         Cargo aux =(Cargo)instance.buscarCargoPorNombre("test cajero",pro);
-        Cargo elCargo2 = new Cargo(64,"cajero");        
+            
         assertTrue(elCargo.equal(aux));
+        instance.eliminarCargo(elCargo, pro);
+      
         //assertTrue(elCargo.equal(elCargo2));
         //TODO: poner unique al campo de nombre de la categoria
        // assertEquals(elCargo2, aux);// comparo con un cargo distinto a ver si sirve
@@ -66,60 +68,107 @@ public class CargoDaoImplTest {
     */
     @Test
     public void testEliminarCargo() {
-        System.out.println("eliminarCargo");
+        System.out.println("probando eliminarCargo");
         Cargo aBorrar =  new Cargo(15, "obrero");
         CargoDaoImpl instance = new CargoDaoImpl();
         probador.ProbarHibernate pro = new ProbarHibernate();
         pro.asociarSession();
         instance.salvarCargo(aBorrar, pro);
-        boolean aux =  instance.eliminarCargo(aBorrar);
+        boolean aux =  instance.eliminarCargo(aBorrar, pro);
         assertTrue(aux);
         
-//TODO: hacer las pruebas de actualizar, buscar y listarCargo
-
     }
      
 
     /**
      * Test of actualizarCargo method, of class CargoDaoImpl.
-    
+    */
     @Test
-    public void testActualizarCargo() {
-        System.out.println("actualizarCargo");
-        Cargo aActualizar = null;
+    public void testActualizarCargo(){
+        System.out.println("probando actualizarCargo");
+        Cargo aActualizar = new Cargo(43,"cargo a actualizar");
         CargoDaoImpl instance = new CargoDaoImpl();
-        instance.actualizarCargo(aActualizar);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        probador.ProbarHibernate pro = new ProbarHibernate();
+        pro.asociarSession();
+        instance.salvarCargo(aActualizar,pro);//salvo el cargo
+        Cargo aux = (Cargo)instance.buscarCargoPorId(43 , pro);
+        assertNotNull(aux);//compruebo que sea encontrado el objeto
+        aux.setCargoDescripcion("cargo actualizado");
+        instance.actualizarCargo(aux, pro);
+        Cargo aux2 = (Cargo)instance.buscarCargoPorId(43, pro);
+        assertNotNull(aux2);//compruebo que sea encontrado el objeto
+        assertTrue(aux2.getIdCargo()==aux.getIdCargo());//comparo que el cambio se haya hecho efectivo
+        instance.eliminarCargo(aux2, pro);
+        instance.eliminarCargo(aux, pro);
+        
     }
- */
+ 
     /**
      * Test of buscarCargoPorNombre method, of class CargoDaoImpl.
-    
+     */
     @Test
     public void testBuscarCargoPorNombre() {
-        System.out.println("buscarCargoPorNombre");
-        int nombre = 25;
+        System.out.println("probando buscarCargoPorNombre");
+        probador.ProbarHibernate pro = new ProbarHibernate();
+        pro.asociarSession();
         CargoDaoImpl instance = new CargoDaoImpl();
-        Cargo expResult = null;
-        Cargo result = instance.buscarCargoPorNombre("nombre",new ProbarHibernate());
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Cargo expResult = new Cargo(100,"objeto prueba");
+        instance.salvarCargo(expResult, pro);
+        Cargo result = instance.buscarCargoPorNombre("objeto prueba",pro);
+        assertTrue(expResult.equal(result));
+        instance.eliminarCargo(expResult, pro);
+        
     }
- */
+
     /**
      * Test of listaCargo method, of class CargoDaoImpl.
-    
+     */
     @Test
     public void testListaCargo() {
-        System.out.println("listaCargo");
+
+        System.out.println("probando listaCargo");
+        Cargo car1 = new Cargo(500, "car1");
+        Cargo car2 = new Cargo(600, "car2");
+        Cargo car3 = new Cargo(700, "car3");
+        probador.ProbarHibernate pro = new ProbarHibernate();
+        pro.asociarSession();
         CargoDaoImpl instance = new CargoDaoImpl();
-        List expResult = null;
-        List result = instance.listaCargo();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    } */
+        instance.salvarCargo(car1, pro);
+        instance.salvarCargo(car2, pro);
+        instance.salvarCargo(car3, pro);
+        List<Cargo> result = instance.listaCargo(pro);
+        int x=0;
+        Cargo arreglo[]= new Cargo[3];
+        for (Cargo cargo : result) {
+            arreglo[x]=cargo;
+            x++;
+        }
+        assertTrue(arreglo[0].equal(car1));
+        assertTrue(arreglo[1].equal(car2));
+        assertTrue(arreglo[2].equal(car3));
+        instance.eliminarCargo(car1, pro);
+        instance.eliminarCargo(car2, pro);
+        instance.eliminarCargo(car3, pro);
+     
+    }
+
+  /**
+     * Test of buscarCargoPorId method, of class CargoDaoImpl.
+     */
+    @Test
+    public void testBuscarCargoPorId() {
+        System.out.println("probando buscarCargoPorNombre");
+        probador.ProbarHibernate pro = new ProbarHibernate();
+        pro.asociarSession();
+        CargoDaoImpl instance = new CargoDaoImpl();
+        Cargo expResult = new Cargo(100,"objeto prueba");
+        instance.salvarCargo(expResult, pro);
+        Cargo result = instance.buscarCargoPorId(100,pro);
+        assertTrue(expResult.equal(result));
+        instance.eliminarCargo(result, pro);
+        
+    }
+
+
 
 }
